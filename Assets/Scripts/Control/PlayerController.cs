38109f0ae56fe791d8fd8276d31cec2792c051f6
Clyde Lu <92775935+p42777@users.noreply.github.com>
2022-07-08 00:10:54 +0800

    Initial commit

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using RPG.Movement;
+using RPG.Combat;
+
+// script responsible for RayCasts
+
+namespace RPG.Control{
+    public class PlayerController : MonoBehaviour{
+
+        private void Update(){
+            
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat(){
+
+            RaycastHit[] hits = Physics.RaycastAll(getMouseRay());
+            foreach (RaycastHit hit in hits){
+
+                // check whether we hit a combat component and start attacking it
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>(); // available for both RigidBody and Collider
+                if(target == null){
+                    continue;
+                }
+                if(Input.GetMouseButtonDown(0)){
+                    GetComponent<Fighter>().Attack(target);
+
+                }
+
+            }
+        }
+
+        private void InteractWithMovement(){
+            if (Input.GetMouseButton(0)){
+
+                MoveToCursor();
+            }
+        }
+
+        private void MoveToCursor(){
+
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast((Ray)getMouseRay(), out hit);
+            if (hasHit == true){
+                
+                // change Nav Mesh Agent's destination to the impact point of Raycast hit
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+        }
+
+        private static Ray getMouseRay(){
+
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// script to control the Nav Mesh Agent
+
+namespace RPG.Movement{
+    
+    public class Mover : MonoBehaviour{
+    
+        [SerializeField] Transform target;
+
+        void Update(){
+
+            UpdateAnimator();
+        }
+
+
+        public void MoveTo(Vector3 destination){
+            
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+
+        private void UpdateAnimator(){
+            
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity; // global velocity
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity); // convert to local velocity
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+    }
+}
